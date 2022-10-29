fe9e4431119139db4f3f84a6145a9a53fcb04f55
Luis Murilo <luiszkm@gmail.com>
2022-10-29 09:58:11 -0300

    creat app

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+
