b8b657568432c71d282d6439dca5b563ca5744ed
luis <luiszkm@gmail.com>
2022-11-01 12:42:35 -0300

    add more translations

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,8 +8,11 @@ import { Button } from "../components/Button";
 import { CountrySelector } from "../components/CountrySelector";
 import { FormEvent, useState } from "react";
 import { AiOutlineMail, AiOutlinePhone, AiOutlineUser } from "react-icons/ai";
+import { useTranslation } from "react-i18next";
 
 export function Contact() {
+  const { t } = useTranslation()
+
   const [country, setCountry] = useState<string>('')
   const [invalidCountry, setInvalidCountry] = useState<boolean>(false)
 
@@ -74,59 +77,59 @@ export function Contact() {
   return (
     <Section id="contact">
 
-      <h2>Contato</h2>
+      <h2>{t("Contact")}</h2>
 
 
       <form onSubmit={(e) => handleFormValidation(e)} className="flex flex-col gap-4">
 
         {
           formValid ?
-            <span className="text-cyan-500">Mensagem enviada com sucesso! </span> :
+            <span className="text-cyan-500">{t("SuccessMensagem")} </span> :
             <span className={invalidName || invalidEmail || invalidPhone ? "text-red-700" : "text-black"}>
-              Por favor preencha todos os campos!</span>
+              {t("Fields")}</span>
         }
 
-        <label htmlFor="name"> Nome <span className={invalidName ? "text-red-700" : "text-black"}>*</span>
+        <label htmlFor="name">{t("Name")} <span className={invalidName ? "text-red-700" : "text-black"}>*</span>
           <Input id="name"
             required
             value={name}
-            placeholder="Nome"
-            title={"Nome invalido"}
+            placeholder={t("Name")}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
             icon={<AiOutlineUser />} />
 
-          {invalidName ? <small className="text-red-700">{"nome invalido"}</small> : ""}
+          {invalidName ? <small className="text-red-700">{t("InvalidName")}</small> : ""}
         </label>
 
-        <label htmlFor="email"> Email<span className={invalidEmail ? "text-red-700" : "text-black"}>*</span>
+        <label htmlFor="email"> {t("Email")}<span className={invalidEmail ? "text-red-700" : "text-black"}>*.
+        
+        </span>
           <Input
             required
             value={email}
             placeholder="Email"
             type="email"
             pattern={"[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,4}"}
-            title={"E-mail invalido"}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
             icon={<AiOutlineMail />} />
-          {invalidEmail ? <small className="text-red-700">{"email invalido"}</small> : ""}
+          {invalidEmail ? <small className="text-red-700">{t("InvalidEmail")}</small> : ""}
 
         </label>
 
-        <label htmlFor="phone">Telefone<span className={invalidPhone ? "text-red-700" : "text-black"}>*</span>
+        <label htmlFor="phone">{t("Phone")}<span className={invalidPhone ? "text-red-700" : "text-black"}>*</span>
           <Input required
             id="phone"
             value={phone}
             placeholder="55 31 98765-3210"
-            title={"telefone invalido"}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhone(e.target.value)}
             icon={<AiOutlinePhone />} />
 
-          {invalidPhone ? <small className="text-red-700">{"telefone invalido"}</small> : ""}
+          {invalidPhone ? <small className="text-red-700">{t("InvalidPhone")}</small> : ""}
 
         </label>
 
-        <label htmlFor="message" className="flex flex-col"> Deixe sua mensagem:
-          <textarea id="message" rows={4} placeholder={"sua mensagem"} value={message}
+        <label htmlFor="message" className="flex flex-col"> {t("Message")}
+          <textarea id="message" rows={4} placeholder={t("Message")}
+          value={message}
             onChange={(e) => setMessage(e.target.value)}
             className="h-64 p-2 outline-0 border-2 border-black rounded focus:ring-cyan-500 focus:border-cyan-500">
 
@@ -136,12 +139,12 @@ export function Contact() {
         <label htmlFor="remember" className='flex items-center gap-2 text-2xs'>
           <Checkbox id='remember' required />
           <span className={"text-black"}>
-            Eu concordo com a Política de Privacidade.*</span>
+            {t("IAgre")}*</span>
         </label>
 
         <label htmlFor="country" className='flex items-center flex-col gap-2 text-xs'>
           <span className={invalidCountry ? "text-red-700" : "text-black"}>
-            Selecione seu pais.*</span>
+            {t("YourCountry")}.*</span>
 
           <CountrySelector id="country" value={country}
             className={invalidCountry ? "border-red-700 border-2 rounded " : "border-black border-2 rounded focus:border-cyan-500"}
@@ -151,7 +154,7 @@ export function Contact() {
 
         <ReCAPTCHAForm />
 
-        <Button>Enviar</Button>
+        <Button>{t("Send")}</Button>
 
 
       </form>
